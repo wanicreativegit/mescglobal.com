27e75657eb7985e010f31f615d23d10b67e99ded
wanicreativegit <d.ambrazevicius@wanicreative.com>
2017-05-22 10:02:35 +0100

    update of web

--- wp-content/themes/mesc/footer.php
+++ wp-content/themes/mesc/footer.php
@@ -16,8 +16,9 @@
                     <?php
                     $fb = get_field('facebook', 13);
                     $tw = get_field('twitter', 13);
+                    $lin = get_field('linkedin', 13);
 
-                    if($fb || $tw){
+                    if($fb || $tw || $lin){
 
                         echo '<div class="social">';
 
@@ -29,6 +30,11 @@
                         if($tw){
                             echo '<a href="'.$tw.'">';
                             echo '<div class="tw"></div>';
+                            echo '</a>';
+                        }
+                        if($lin){
+                            echo '<a href="'.$lin.'">';
+                            echo '<div class="lin"></div>';
                             echo '</a>';
                         }
 
@@ -63,6 +69,17 @@
     </div>
 
 </footer>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-92013935-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 
 	</body>
 </html>
--- wp-content/themes/mesc/header.php
+++ wp-content/themes/mesc/header.php
@@ -7,8 +7,8 @@
         } ?> <?php bloginfo('name'); ?></title>
 
     <link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/mesc-icon-2.png" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/mesc-icon-2.png" rel="apple-touch-icon-precomposed">
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
